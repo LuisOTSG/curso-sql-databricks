b94b1ac50d756f19191fed3cd6fb44153042d97a
LuisOTSG <LuisOTSG>
2022-12-15 01:04:16 +0000

    Anotando os exercícios a serem resolvidos

---
+++ Dia_01/Exercícios/EXERCICIOS - PARTE 02.sql
@@ -0,0 +1,34 @@
+-- Databricks notebook source
+-- DBTITLE 1,Exercício Tempo em Foco (WHERE)
+-- 01: Lista de pedidos com mais de um item
+
+-- COMMAND ----------
+
+-- 02: Lista de pedidos que o frete é mais caro que o item
+
+-- COMMAND ----------
+
+-- 03: Lista de pedidos que ainda não foram enviados
+
+SELECT *
+FROM silver_olist.pedido
+
+-- COMMAND ----------
+
+-- 04: Lista de pedidos que foram entregues com atraso
+
+-- COMMAND ----------
+
+-- 05: Lista de pedidos que foram entregues com 2 dias de antecedência
+
+-- COMMAND ----------
+
+-- 06: Lista de pedidos feitos em dezembro de 2017 e entregues com atraso
+
+-- COMMAND ----------
+
+-- 07: Lista de pedidos com avaliação maior ou igual que 4
+
+-- COMMAND ----------
+
+-- 08: Lista de pedidos com 2 ou mais parcelas menores que R$20,00
