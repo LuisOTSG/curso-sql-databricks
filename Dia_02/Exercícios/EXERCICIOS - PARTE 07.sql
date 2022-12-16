c25941b6a05132b98afdcdfc1787df19cc07022c
LuisOTSG <LuisOTSG>
2022-12-16 01:57:49 +0000

    Anotando os exercícios da parte 07

---
+++ Dia_02/Exercícios/EXERCICIOS - PARTE 07.sql
@@ -0,0 +1,28 @@
+-- Databricks notebook source
+-- DBTITLE 1,Tempo de Foco (AGREGAÇÕES)
+-- 01: Quantos vendedores são do estado de São Paulo?
+
+
+-- COMMAND ----------
+
+-- 02: Quantos vendedores são de Presidente Prudente?
+
+-- COMMAND ----------
+
+-- 03: Quantos clientes são do estado do Rio de Janeiro?
+
+-- COMMAND ----------
+
+-- 04: Quantos produtos são de construção?
+
+-- COMMAND ----------
+
+-- 05: Qual o valor médio de um pedido? E do frete?
+
+-- COMMAND ----------
+
+-- 06: Em média os pedidos são de quantas parcelas de cartão? E o valor médio por parcela?
+
+-- COMMAND ----------
+
+-- 07: Quanto tempo em média demora para um pedido chegar depois de aprovado?
