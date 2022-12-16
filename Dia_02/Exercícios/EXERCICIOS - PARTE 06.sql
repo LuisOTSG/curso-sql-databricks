2d68d03edb9437c1046881de2f2f0d4493a7728d
LuisOTSG <LuisOTSG>
2022-12-16 01:57:43 +0000

    Anotando os exercícios da parte 06

---
+++ Dia_02/Exercícios/EXERCICIOS - PARTE 06.sql
@@ -0,0 +1,15 @@
+-- Databricks notebook source
+-- DBTITLE 1,Exercícios Parte 06 - Bora praticar?
+-- 01: Qual categoria tem mais produtos vendidos?
+
+-- COMMAND ----------
+
+-- 02: Qual categoria tem produtos mais caros, em média?
+
+-- COMMAND ----------
+
+-- 03: Qual categoria tem maiores fretes, em média?
+
+-- COMMAND ----------
+
+-- 04: Os clientes de qual estado pagam mais frete?
