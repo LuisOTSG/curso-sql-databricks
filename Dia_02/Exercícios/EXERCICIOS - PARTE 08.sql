1b823e913912dfcb0b0244d1f8fd6909175cebaa
LuisOTSG <LuisOTSG>
2022-12-16 01:57:54 +0000

    Anotando os exercícios da parte 08

---
+++ Dia_02/Exercícios/EXERCICIOS - PARTE 08.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+-- DBTITLE 1,Tempo de Foco (GROUP BY)
+-- 01: Qual estado tem mais vendedores?
+
+-- COMMAND ----------
+
+-- 02: Qual cidade tem mais clientes?
+
+-- COMMAND ----------
+
+-- 03: Existe diferença entre os valores de pedido em relação aos meios de pagamento?
+
+-- COMMAND ----------
+
+-- 04: Qual categoria tem mais itens?
+
+-- COMMAND ----------
+
+-- 05: Qual categoria tem maior peso médio de produto?
+
+-- COMMAND ----------
+
+-- 06: Qual a série histórica de pedidos por dia? E receita?
